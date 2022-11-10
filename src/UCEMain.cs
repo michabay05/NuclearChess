26904a0bc43bbd5a40b1e651f621f4582561078c
michabay05 <111288777+michabay05@users.noreply.github.com>
2022-11-09 21:00:15 -0500

    Modified evaluation to add Position square Tables

--- src/UCEMain.cs
+++ src/UCEMain.cs
@@ -20,8 +20,19 @@ class UCEMain
     static void Test()
     {
         string fen = "6k1/ppppprbp/8/8/8/8/PPPPPRBP/6K1 w - - 0 1";
+        fen = FEN.position[1];
         Board a = FEN.Parse(fen);
         a.Display();
         Console.WriteLine($"Score: {Eval.Evaluate(ref a)}");
+        /*
+        for (int r = 0; r < 8; r++)
+        {
+            for (int f = 0; f < 8; f++)
+            {
+                Console.Write(BoardUtil.GetRank(r * 8 + f) + " ");
+            }
+            Console.WriteLine();
+        }
+        */
     }
 }
