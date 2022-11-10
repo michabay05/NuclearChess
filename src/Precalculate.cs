e3b372d04143dc6e562313b47aa1664aca6bcc47
michabay05 <111288777+michabay05@users.noreply.github.com>
2022-11-10 16:23:50 +0000

    Modified rank and file util

--- src/Precalculate.cs
+++ src/Precalculate.cs
@@ -94,16 +94,16 @@ class Precalculate
     {
         if (color == 0) // WHITE
         {
-            if (BoardUtil.ranks[sq] < 7 && BoardUtil.files[sq] > 0)
+            if (BoardUtil.GetRank(sq) < 7 && BoardUtil.GetFile(sq) > 0)
                 BitUtil.SetBit(ref pawnAttacks[color, sq], sq + (int)Direction.SW);
-            if (BoardUtil.ranks[sq] < 7 && BoardUtil.files[sq] < 7)
+            if (BoardUtil.GetRank(sq) < 7 && BoardUtil.GetFile(sq) < 7)
                 BitUtil.SetBit(ref pawnAttacks[color, sq], sq + (int)Direction.SE);
         }
         else // BLACK
         {
-            if (BoardUtil.ranks[sq] > 0 && BoardUtil.files[sq] > 0)
+            if (BoardUtil.GetRank(sq) > 0 && BoardUtil.GetFile(sq) > 0)
                 BitUtil.SetBit(ref pawnAttacks[color, sq], sq + (int)Direction.NW);
-            if (BoardUtil.ranks[sq] > 0 && BoardUtil.files[sq] < 7)
+            if (BoardUtil.GetRank(sq) > 0 && BoardUtil.GetFile(sq) < 7)
                 BitUtil.SetBit(ref pawnAttacks[color, sq], sq + (int)Direction.NE);
         }
     }
@@ -111,41 +111,41 @@ class Precalculate
     public static void GenKnightAttacks(int sq)
     {
         // TODO: KNIGHT MOVES FOR G1 - NOT WORKING
-        if (BoardUtil.ranks[sq] <= 5 && BoardUtil.files[sq] >= 1)
+        if (BoardUtil.GetRank(sq) <= 5 && BoardUtil.GetFile(sq) >= 1)
             BitUtil.SetBit(ref knightAttacks[sq], sq + (int)Direction.SW_S);  // - 17
-        if (BoardUtil.ranks[sq] <= 6 && BoardUtil.files[sq] >= 2)
+        if (BoardUtil.GetRank(sq) <= 6 && BoardUtil.GetFile(sq) >= 2)
             BitUtil.SetBit(ref knightAttacks[sq], sq + (int)Direction.SW_W);  // - 10
-        if (BoardUtil.ranks[sq] <= 6 && BoardUtil.files[sq] <= 5)
+        if (BoardUtil.GetRank(sq) <= 6 && BoardUtil.GetFile(sq) <= 5)
             BitUtil.SetBit(ref knightAttacks[sq], sq + (int)Direction.SE_E);  // - 6
-        if (BoardUtil.ranks[sq] <= 5 && BoardUtil.files[sq] <= 6)
+        if (BoardUtil.GetRank(sq) <= 5 && BoardUtil.GetFile(sq) <= 6)
             BitUtil.SetBit(ref knightAttacks[sq], sq + (int)Direction.SE_S);  // - 15
-        if (BoardUtil.ranks[sq] >= 2 && BoardUtil.files[sq] <= 6)
+        if (BoardUtil.GetRank(sq) >= 2 && BoardUtil.GetFile(sq) <= 6)
             BitUtil.SetBit(ref knightAttacks[sq], sq + (int)Direction.NE_N);  // + 17
-        if (BoardUtil.ranks[sq] >= 1 && BoardUtil.files[sq] <= 5)
+        if (BoardUtil.GetRank(sq) >= 1 && BoardUtil.GetFile(sq) <= 5)
             BitUtil.SetBit(ref knightAttacks[sq], sq + (int)Direction.NE_E);  // + 10
-        if (BoardUtil.ranks[sq] >= 1 && BoardUtil.files[sq] >= 2)
+        if (BoardUtil.GetRank(sq) >= 1 && BoardUtil.GetFile(sq) >= 2)
             BitUtil.SetBit(ref knightAttacks[sq], sq + (int)Direction.NW_W);  // + 6
-        if (BoardUtil.ranks[sq] >= 2 && BoardUtil.files[sq] >= 1)
+        if (BoardUtil.GetRank(sq) >= 2 && BoardUtil.GetFile(sq) >= 1)
             BitUtil.SetBit(ref knightAttacks[sq], sq + (int)Direction.NW_N);  // + 15
     }
 
     public static void GenKingAttacks(int sq)
     {
-        if (BoardUtil.ranks[sq] > 0)
+        if (BoardUtil.GetRank(sq) > 0)
             BitUtil.SetBit(ref kingAttacks[sq], sq + (int)Direction.N);
-        if (BoardUtil.ranks[sq] < 7)
+        if (BoardUtil.GetRank(sq) < 7)
             BitUtil.SetBit(ref kingAttacks[sq], sq + (int)Direction.S);
-        if (BoardUtil.files[sq] > 0)
+        if (BoardUtil.GetFile(sq) > 0)
             BitUtil.SetBit(ref kingAttacks[sq], sq + (int)Direction.W);
-        if (BoardUtil.files[sq] < 7)
+        if (BoardUtil.GetFile(sq) < 7)
             BitUtil.SetBit(ref kingAttacks[sq], sq + (int)Direction.E);
-        if (BoardUtil.ranks[sq] > 0 && BoardUtil.files[sq] > 0)
+        if (BoardUtil.GetRank(sq) > 0 && BoardUtil.GetFile(sq) > 0)
             BitUtil.SetBit(ref kingAttacks[sq], sq + (int)Direction.NW);
-        if (BoardUtil.ranks[sq] > 0 && BoardUtil.files[sq] < 7)
+        if (BoardUtil.GetRank(sq) > 0 && BoardUtil.GetFile(sq) < 7)
             BitUtil.SetBit(ref kingAttacks[sq], sq + (int)Direction.NE);
-        if (BoardUtil.ranks[sq] < 7 && BoardUtil.files[sq] > 0)
+        if (BoardUtil.GetRank(sq) < 7 && BoardUtil.GetFile(sq) > 0)
             BitUtil.SetBit(ref kingAttacks[sq], sq + (int)Direction.SW);
-        if (BoardUtil.ranks[sq] < 7 && BoardUtil.files[sq] < 7)
+        if (BoardUtil.GetRank(sq) < 7 && BoardUtil.GetFile(sq) < 7)
             BitUtil.SetBit(ref kingAttacks[sq], sq + (int)Direction.SE);
     }
 
--- src/UCEMain.cs
+++ src/UCEMain.cs
@@ -2,7 +2,7 @@
 
 class UCEMain
 {
-    private static int debug = 0;
+    private static int debug = 1;
     static void Main(string[] args)
     {
         BoardUtil.InitRankFileMasks();
@@ -19,10 +19,15 @@ class UCEMain
 
     static void Test()
     {
-        string fen = "7r/8/8/8/8/8/8/R7 w - - 0 1";
-        //fen = FEN.position[1];
-        Board a = FEN.Parse(fen);
-        a.Display();
-        Console.WriteLine($"Score: {Eval.Evaluate(ref a)}");
+        for (int r = 0; r < 8; r++)
+        {
+            for (int f = 0; f < 8; f++)
+            {
+                int sq = r * 8 + f;
+                Console.WriteLine((Squares)sq);
+                BitUtil.PrintBits(Precalculate.pawnAttacks[0, sq]);
+            }
+            Console.WriteLine();
+        }
     }
 }
